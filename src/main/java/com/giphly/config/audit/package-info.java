a2b106b2918d34fae11e96991140d668575529dc
Mark Buyukisik <buyukim@gmail.com>
2020-02-10 01:30:31 -0600

    Initial version of giphly generated by JHipster-6.7.0

---
+++ src/main/java/com/giphly/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.giphly.config.audit;
---
+++ src/main/webapp/app/admin/metrics/metrics.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { GiphlySharedModule } from 'app/shared/shared.module';
+
+import { MetricsComponent } from './metrics.component';
+
+import { metricsRoute } from './metrics.route';
+
+@NgModule({
+  imports: [GiphlySharedModule, RouterModule.forChild([metricsRoute])],
+  declarations: [MetricsComponent]
+})
+export class MetricsModule {}
