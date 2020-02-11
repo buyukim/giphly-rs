6bbea2e1ec55b8b46ec8428c6d63a6a4ee457643
Mark Buyukisik <buyukim@gmail.com>
2020-02-10 22:17:29 -0600

    setting better default options for the jackson mapper and only making sure that one MappingJackson2HttpMessageConverter is being set (there was currently two)

--- src/main/java/com/giphly/config/RestTemplateConfiguration.java
+++ src/main/java/com/giphly/config/RestTemplateConfiguration.java
@@ -1,5 +1,14 @@
 package com.giphly.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import com.giphly.client.giphy.api.invoker.RFC3339DateFormat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +22,15 @@ import java.util.Arrays;
 @Configuration
 public class RestTemplateConfiguration {
 
+    @Autowired
+    private JavaTimeModule javaTimeModule;
+
+    @Autowired
+    private Jdk8Module jdk8Module;
+
+    @Autowired
+    private AfterburnerModule afterburnerModule;
+
     @Bean
     public RestTemplate giphyRestTemplate() {
         Duration readTimeout = Duration.ofSeconds(20L);
@@ -20,15 +38,27 @@ public class RestTemplateConfiguration {
 
         MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter =
             new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = mappingJackson2HttpMessageConverter.getObjectMapper()
+            .registerModule(afterburnerModule)
+            .registerModule(jdk8Module)
+            .registerModule(javaTimeModule);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+        mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+        mapper.setDateFormat(new RFC3339DateFormat());
+        mappingJackson2HttpMessageConverter.setObjectMapper(mapper);
         mappingJackson2HttpMessageConverter.setSupportedMediaTypes(
             Arrays.asList(
                 MediaType.APPLICATION_JSON,
                 MediaType.APPLICATION_OCTET_STREAM));
-      //  restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
-        return new RestTemplateBuilder()
+       RestTemplate restTemplate = new RestTemplateBuilder()
             .setReadTimeout(readTimeout)
             .setConnectTimeout(connectTimeout)
-            .defaultMessageConverters().additionalMessageConverters(mappingJackson2HttpMessageConverter)
+            .additionalMessageConverters(mappingJackson2HttpMessageConverter)
             .build();
+       return restTemplate;
     }
 }
